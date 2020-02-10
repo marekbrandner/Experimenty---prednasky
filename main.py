13b87eda7c4254e4b8afddf557893492ce99863c
marekbrandner <brandner@kma.zcu.cz>
2020-02-10 08:25:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import matplotlib.pyplot as plt
 import lecture1
 
 def pokus():
-#    lecture1.residuum(10)
-    t = numpy.arange(0., 10., 0.2)
-    plt.plot(t, 1./numpy.tanh(t) - 1./t , 'r--')
-    plt.show()
+    lecture1.residuum(10)
+    
 if __name__ == "__main__":
     pokus()
